d884fb0fce8eb152ef89b1469cc266375ea9d9a7
Parham-F <parham.parvizfarzam@gmail.com>
2025-11-13 23:21:24 -0700

    Exercise 13.3

---
+++ cli.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { Sequelize, QueryTypes } from 'sequelize'
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+    try {
+        await sequelize.authenticate()
+        const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+        blogs.map(blog => console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`))
+        sequelize.close()
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
+}
+
+main()
